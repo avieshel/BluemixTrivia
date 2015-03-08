d9bf1e9368eccf419a0b6e441dcd7371af4db4e0
Itay Zror <itayz@Itays-Mac-mini.haifa.ibm.com>
2015-03-08 14:38:37 +0200

    Add game UI

---
+++ BluemixTrivia/BluemixTrivia/GameViewContorller.h
@@ -0,0 +1,13 @@
+//
+//  GameViewContorller.h
+//  BluemixTrivia
+//
+//  Created by Itay Zror on 3/8/15.
+//  Copyright (c) 2015 Avi Eshel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewContorller : UIViewController
+
+@end
