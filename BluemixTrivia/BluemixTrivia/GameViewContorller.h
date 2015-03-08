8499e38ef1d261a64f4f7ba4500a5f212addedef
Itay Zror <itayz@Itays-Mac-mini.haifa.ibm.com>
2015-03-08 16:08:31 +0200

    change ui

--- BluemixTrivia/BluemixTrivia/GameViewContorller.h
+++ BluemixTrivia/BluemixTrivia/GameViewContorller.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface GameViewContorller : UIViewController
+@interface GameViewContorller : UIViewController<UITableViewDataSource,UITableViewDelegate>
 
 @end
--- BluemixTrivia/BluemixTrivia/GameViewContorller.m
+++ BluemixTrivia/BluemixTrivia/GameViewContorller.m
@@ -11,13 +11,79 @@
 @interface GameViewContorller ()
 
 @property (weak, nonatomic) IBOutlet UILabel *questionLbl;
-
+@property NSArray *ansArray;
+@property (weak, nonatomic) IBOutlet UITableView *ansTable;
 
 @end
 
 
 @implementation GameViewContorller
 
+
+- (void)viewDidLoad {
+
+    [super viewDidLoad];
+    self.ansArray = @[@"David Ben Gurion",@"Moshe Sharet",@"Ehud Barak",@"Benjamin Netanyahu"];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+#pragma mark - Table View Data source
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:
+(NSInteger)section{
+    return self.ansArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:
+(NSIndexPath *)indexPath{
+    static NSString *cellIdentifier = @"cellID";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:
+                             cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:
+                UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    NSString *stringForCell;
+    stringForCell= [self.ansArray objectAtIndex:indexPath.row];
+    [cell.textLabel setText:stringForCell];
+    return cell;
+}
+
+// Default is 1 if not implemented
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:
+(NSInteger)section{
+    NSString *headerTitle;
+    headerTitle = @"";
+    return headerTitle;
+}
+- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:
+(NSInteger)section{
+    NSString *footerTitle;
+    footerTitle = @"";
+    return footerTitle;
+}
+
+#pragma mark - TableView delegate
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:
+(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    NSLog(@"Section:%d Row:%d selected and its data is %@",
+          indexPath.section,indexPath.row,cell.textLabel.text);
+}
 
 
 @end
